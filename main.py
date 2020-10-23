39362d1cc015bc1326e0bc1594a2eae42af114ea
root <root@localhost.localdomain>
2020-10-23 15:51:05 -0300

    Add project to alter ipv4 settings

---
+++ main.py
@@ -0,0 +1,78 @@
+import json
+import requests
+
+BASE_URL = 'http://10.0.0.10/cgi-bin'
+API = {
+    'login': BASE_URL + '/api/v3/system/login',
+    'lan': BASE_URL + '/api/v3/interface/lan/id',
+    'apply': BASE_URL + '/api/v3/system/apply',
+    'apply_status': BASE_URL + '/api/v3/system/apply/status'
+}
+headers = {'content-type': 'application/json'}
+
+
+def auth(username, password):
+    data = {'data': {'username': username, 'password': password}}
+    response = requests.post(API['login'], data=json.dumps(data),
+                             headers=headers, verify=False)
+    if response.status_code == 200:
+        token1 = response.content.decode('utf-8')
+
+        token = json.loads(response.content.decode('utf-8'))['data']['Token']
+        headers['Authorization'] = 'Token ' + token
+        return True
+
+    return False
+
+
+def update_ipv4(ipv4_settings):
+    response = requests.put(API['lan'], data=json.dumps(ipv4_settings),
+                            headers=headers, verify=False)
+    if response.status_code == 204:
+        return True
+    return False
+
+
+def get_ipv4():
+    response = requests.get(API['lan'], headers=headers, verify=False)
+    if response.status_code == 200:
+        ipv4_settings = json.loads(response.content.decode('utf-8'))
+        return ipv4_settings
+    return None
+
+
+def apply_changes():
+    response = requests.post(API['apply'], headers=headers, verify=False)
+    if response.status_code == 200:
+        success = json.loads(response.content.decode('utf-8'))['data']['success']
+        return success
+    return None
+
+
+def has_changes():
+    response = requests.get(API['apply_status'], headers=headers, verify=False)
+    if response.status_code == 200:
+        has_update = json.loads(response.content.decode('utf-8'))['data']['has_update']
+        return has_update
+    return None
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    username = 'admin'
+    password = 'admin'
+
+    new_ip = '10.0.0.1'
+
+    auth(username, password)
+    ipv4_settings = get_ipv4()
+
+    if ipv4_settings is not None:
+        ipv4_settings['data']['ip_address'] = new_ip
+        update_ipv4(ipv4_settings)
+
+
+    if has_changes():
+        apply_changes()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
