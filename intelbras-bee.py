e9c0ad322692fb53ee8eba3febbb43ba3a7cffbe
root <root@localhost.localdomain>
2020-10-23 17:20:31 -0300

    Add method to verify changes using ping

--- intelbras-bee.py
+++ intelbras-bee.py
@@ -1,7 +1,10 @@
 import json
 import requests
+import os
 
-BASE_URL = 'http://10.0.0.10/cgi-bin'
+from time import sleep
+
+BASE_URL = 'http://10.0.0.1/cgi-bin'
 API = {
     'login': BASE_URL + '/api/v3/system/login',
     'lan': BASE_URL + '/api/v3/interface/lan/id',
@@ -12,13 +15,13 @@ headers = {'content-type': 'application/json'}
 
 
 def auth(username, password):
-    """Takes a username and password, returns an authentication token"""
+    """
+    Takes a username and password, returns an authentication token
+    """
     data = {'data': {'username': username, 'password': password}}
     response = requests.post(API['login'], data=json.dumps(data),
                              headers=headers, verify=False)
     if response.status_code == 200:
-        token1 = response.content.decode('utf-8')
-
         token = json.loads(response.content.decode('utf-8'))['data']['Token']
         headers['Authorization'] = 'Token ' + token
         return True
@@ -29,7 +32,6 @@ def auth(username, password):
 def update_ipv4(ipv4_settings):
     """
     Takes in a dict with ipv4 settings, and updates it to the AP
-    Returns nothing
     """
     response = requests.put(API['lan'], data=json.dumps(ipv4_settings),
                             headers=headers, verify=False)
@@ -39,7 +41,9 @@ def update_ipv4(ipv4_settings):
 
 
 def get_ipv4():
-    """Gets the ipv4 settings from the AP"""
+    """
+    Gets the ipv4 settings from the AP
+    """
     response = requests.get(API['lan'], headers=headers, verify=False)
     if response.status_code == 200:
         ipv4_settings = json.loads(response.content.decode('utf-8'))
@@ -48,7 +52,9 @@ def get_ipv4():
 
 
 def apply_changes():
-    """Applies any saved changes to the AP"""
+    """
+    Applies any saved changes to the AP
+    """
     response = requests.post(API['apply'], headers=headers, verify=False)
     if response.status_code == 200:
         success = json.loads(response.content.decode('utf-8'))['data']['success']
@@ -57,7 +63,9 @@ def apply_changes():
 
 
 def has_changes():
-    """Verifies whether or not there are saved changes that are not applied"""
+    """
+    Verifies whether or not there are saved changes that are not applied
+    """
     response = requests.get(API['apply_status'], headers=headers, verify=False)
     if response.status_code == 200:
         has_update = json.loads(response.content.decode('utf-8'))['data']['has_update']
@@ -65,12 +73,22 @@ def has_changes():
     return None
 
 
-# Press the green button in the gutter to run the script.
+def verify_changes():
+    """
+    Verifies if the ipv4 settings were changed using ping
+    """
+    response = os.system('ping -c 1 ' + new_ip)
+    if response == 0:
+        return True
+    else:
+        return False
+
+
 if __name__ == '__main__':
     username = 'admin'
     password = 'admin'
 
-    new_ip = '10.0.0.1'
+    new_ip = '10.0.0.10'
 
     auth(username, password)
     ipv4_settings = get_ipv4()
@@ -79,8 +97,14 @@ if __name__ == '__main__':
         ipv4_settings['data']['ip_address'] = new_ip
         update_ipv4(ipv4_settings)
 
-
     if has_changes():
         apply_changes()
+        print('Device is restarting...')
+        sleep(40)  # measurements indicate that the device takes about 36 seconds to restart.
+
+    if verify_changes():
+        print('New access point IPv4 is ' + new_ip)
+    else:
+        print('Couldn\'t change access point IPv4 settings.')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
